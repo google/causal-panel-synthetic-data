1f66d2112ceaaa236ef305ceb9534b36f24e4397
Alex Kellogg <alexkellogg@google.com>
2020-08-18 13:42:08 -0700

    updates to placebo creation script given comments by oldham.

--- r_code/placebo_creation.R
+++ r_code/placebo_creation.R
@@ -1,27 +1,22 @@
-library(tidyverse)
-library(dplyr)
-library(furrr)
+pacman::p_load(dplyr, tidyr, stats, tibble)
 ############################################
 # Functions to create a placebo dataset,
 # assigning treatment based on matching.
 ###########################################
 
 
-nearest_ts_euclidean <- function(ts_tomatch, ts_rest) {
-  # Helper function for matching_without_replacement
-  # Measures the euclidean distance between ts_tomatch
-  # and each of the rows in the tibble ts_rest.
-  
-  # Args
-  # ts_tomatch: treated time series tibble, rows identify ID and columns Time
-  # ts_rest: donor time series tibble, rows identify ID and columns Time.
-  
-  # Output
-  # Vector of indices indicating the row in ts_rest that are best match
-  # (min L2 norm distance) to the row in ts_tomatch.
-  # (Vector length equal to number of rows in ts_tomatch, ie treated entries)
-  # If ts_tomatch has more than 1 entry, the matching is done with replacement
-  # (multiple treated can have the same match).
+.NearestTSEuclidean <- function(ts_tomatch, ts_rest) {
+  #' Helper function for .MatchingWithoutReplacement.
+  #' Measures the euclidean distance between ts_tomatch
+  #' and each of the rows in the tibble ts_rest.
+  #' 
+  #' @param ts_tomatch Treated time series tibble, rows are ID and columns Time.
+  #' @param ts_rest Donor time series tibble, rows identify ID and columns Time.
+  #' 
+  #' @return Vector of indices each indicating the row in ts_rest that is best 
+  #'      matched (min L2 norm distance) to the row in ts_tomatch.
+  #'      
+  #' @noRd
   
   return(apply(ts_tomatch, 1, function(ts_tomatch) {
     which.min(
@@ -36,33 +31,34 @@ nearest_ts_euclidean <- function(ts_tomatch, ts_rest) {
 }
 
 
-matching_without_replacement <- function(treated_block,
+.MatchingWithoutReplacement <- function(treated_block,
                                          control_block,
                                          id_var,
                                          treat_period) {
-  # finds the nearest match for each treated unit (treated_block)
-  # among the donor pool (control_block) without replacement by calling helper
-  # to nearest_ts_euclidean.
+  #' Finds the nearest match for each treated unit (treated_block)
+  #' among the donor pool (control_block) without replacement by calling helper
+  #' .NearestTSEuclidean.
+  #'
+  #' @param treated_block Treated time series tibble, dimension ID by Time.
+  #' @param control_block Donor time series tibble, dimension ID by Time.
+  #' 
+  #' @return A tibble containing a column for the placebo-treated unit ID,
+  #'     the treated unit it was the nearest match to, and the treatment time of 
+  #'     the true treated unit.
+  #' @noRd
   
-  # Args
-  # treated_block: treated time series tibble, rows are ID and columns Time
-  # control_block: donor time series tibble, rows are ID and columns Time.
-  
-  # Output
-  # df_toreturn, a tibble containing a column for the placebo-treated unit 
-  # ID numbers, the treated unit it was the nearest match to, 
-  # and the time that treated unit was actually
-  # treated (num_rows of the dataframe equal to num_rows of treated_block).
-  
-  # initialize an empty vector for the donor IDs that match
+  # Initialize an empty vector for the donor IDs that match.
   already_matched <- c()
-  # Store the time of treatment and treated ID for the true treated units
-  placebo_treat_period <- treated_block %>% dplyr::pull(tidyselect::all_of(treat_period))
+  # Randomize the order of matching for this greedy approach.
+  treated_block <- treated_block %>% dplyr::sample_n(dplyr::n())
+  # Store the time of treatment and treated ID for the true treated units.
+  placebo_treat_period <- treated_block %>% 
+    dplyr::pull(tidyselect::all_of(treat_period))
   treatment_unit <- treated_block %>% dplyr::pull(!!as.name(id_var))
   for (i in seq_len(nrow(treated_block))) {
-    #Find nearest euclidean match among unmatched controls for 
-    #each treated observation
-    temp_match <- nearest_ts_euclidean(
+    # Find nearest euclidean match among unmatched controls for 
+    # each treated observation.
+    temp_match <- .NearestTSEuclidean(
       treated_block %>%
         dplyr::slice(i) %>%
         dplyr::select(-tidyselect::all_of(c(id_var,treat_period))),
@@ -70,14 +66,14 @@ matching_without_replacement <- function(treated_block,
         dplyr::filter(!!as.name(id_var) %in% setdiff(!!as.name(id_var), 
                                               already_matched)) %>% 
         dplyr::select(-tidyselect::all_of(id_var)))
-    #Update the vector of already matched donors
+    # Update the vector of already matched donors.
     already_matched[i] <- control_block %>%
       dplyr::filter(!!as.name(id_var) %in% 
                setdiff(!!as.name(id_var), already_matched)) %>%
       dplyr::slice(temp_match) %>%
       dplyr::pull(!!as.name(id_var))
   }
-  # Store the resulting vectors in a tibble for output
+  # Store the resulting vectors in a tibble for output.
   df_toreturn <- tibble::tibble(temp_id = already_matched, 
                         temp_treattime = placebo_treat_period, 
                         Treatment_Unit = treatment_unit)
@@ -87,41 +83,34 @@ matching_without_replacement <- function(treated_block,
                   !!as.name(treat_period) := temp_treattime))
 }
 
-
-
-#TODO(alexdkellogg): Expand to matching with and storing covariates
-#                   (pivot wide/long, specifically).
-#TODO(alexdkellogg): account for multiple treatments, treatment end dates
-create_placebo_df <- function(data_full, id_var = "entry",
+#TODO(alexdkellogg):  Revisit whether pivoting will cause issues in the 
+#    presence of several time varying covariates.
+#TODO(alexdkellogg): Account for multiple treatments, treatment end dates.
+CreatePlaceboData <- function(data_full, id_var = "entry",
                               time_var = "period", 
                               treat_indicator = "treatperiod_0",
                               outcome_var = "target", 
                               counterfac_var = "counter_factual") {
-  # Generates a placebo tibble, using matching methods to select
-  # placebo-treated entries as those most similar to truly-treated.
+  #' Generates a placebo tibble, using matching methods to select
+  #' placebo-treated entries as those most similar to truly-treated.
+  #'
+  #' @param data_full Long-form tibble with both treated and control entries.
+  #'    rows represent period-entry combinations  (e.g. N (total num of entry) 
+  #'    rows for period t). Each row should have a treatment indicator 
+  #'    (treat_indicator),  a period number (time_var), an individual ID 
+  #'    (id_var), and an outcome (outcome_var) for associated with that 
+  #'    period-ID combination.
+  #' @param id_var Column name of numeric, unique ID representing the entry.
+  #' @param time_var Column name of numeric period number (the time period),
+  #'    in increasing order (e.g. 0 is the first time, 120 is the last).
+  #' @param treat_indicator Column name of binary (0, 1) indicator for whether 
+  #'     the unit in that time period was treated. Should never decrease.
+  #'     
+  #' @return A tibble of the same format as data_full, 
+  #'    entirely consisting of donor units, some of which are placebo-treated 
+  #'    (based on matching).
   
-  # Args
-  # data_full: long-form dataframe with both treated and control entries.
-  #            rows of the df represent period-entry combinations 
-  #            (eg N (total num of entry) rows for period t).
-  #            each row should have a treatment indicator (treat_indicator), 
-  #            a period number (time_var),
-  #            an individual ID (id_var), and an outcome (outcome_var)
-  #            for associated with that period-ID combination
-  # id_var: column name of numeric, unique ID representing the entry (unit)
-  # time_var:column name of numeric period number indicating the time period, 
-  #          in increasing order (eg 0 is the first time, 120 is the last)
-  # treat_indicator:column name of binary (0, 1) indicator for whether id_var i 
-  #                 in time_var t was treated. 
-  #                 Once treated, must always be treated.
-  
-  
-  # Output
-  # placebo_df_long, a tibble of the same format as data_full, entirely
-  # consisting of donor units, some of which are placebo-treated 
-  # (based on matching).
-  
-  # Split the dataset based on whether the unit is ever treated
+  # Split the dataset based on whether the unit is ever treated.
   tr_entries <- data_full %>%
     dplyr::filter(!!as.name(treat_indicator) > 0) %>%
     dplyr::distinct(!!as.name(id_var)) %>%
@@ -130,7 +119,7 @@ create_placebo_df <- function(data_full, id_var = "entry",
   ct_entries <- setdiff(data_full %>%
                           dplyr::distinct(!!as.name(id_var)) %>%
                           dplyr::pull(), tr_entries)
-  # Create a dataframe pf the subset of control units
+  # Create a dataframe of the subset of control units.
   cd <- data_full %>% dplyr::filter(!!as.name(id_var) %in% ct_entries)
   cd <- merge(
     cd,
@@ -139,9 +128,8 @@ create_placebo_df <- function(data_full, id_var = "entry",
       rank = seq_along(ct_entries)
     )
   )
-  
   # Pivot the long cd dataframe to wide. Each row will represent an id_var, 
-  #with columns for the outcome_var at each time period
+  # with columns for the outcome_var at each time period.
   cd_for_match <- tidyr::pivot_wider(
     data = cd %>%
       dplyr::arrange(!!as.name(time_var), !!as.name(id_var)),
@@ -149,12 +137,10 @@ create_placebo_df <- function(data_full, id_var = "entry",
     id_cols = c(!!as.name(id_var)),
     values_from = c(!!as.name(outcome_var))
   )
-  
-  # Store treated data in a tibble
+  # Store treated data in a tibble.
   treated_to_match <- data_full %>%
     dplyr::filter(!!as.name(id_var) %in% tr_entries)
-  
-  #Create a variable indicating the time treatment is assigned by unit
+  # Create a variable indicating the time treatment is assigned by unit.
   treated_to_match <- treated_to_match %>%
     dplyr::group_by(!!as.name(id_var)) %>%
     dplyr::mutate(
@@ -162,28 +148,22 @@ create_placebo_df <- function(data_full, id_var = "entry",
         length(!!as.name(treat_indicator)) - sum(!!as.name(treat_indicator)) + 1
     ) %>%
     dplyr::ungroup()
-  
-  # for pivoting, potential issues arise if we have several time varying 
-  # covariates -- added as TODO.
-  # we'd have to take the values_from each of them, 
-  #and for any constant args we'd presumably have to add them to id_cols
-  #Pivot the data wide, so each row has all the time series data for 
-  #a given unit.
+  # Pivot the data wide, so each row has all the time series data for 
+  # a given unit.
   data_wide_m <- tidyr::pivot_wider(
     data = treated_to_match,
     names_from = !!as.name(time_var),
     id_cols = c(!!as.name(id_var), Treatment_Period),
     values_from = c(!!as.name(outcome_var))
-  ) # %>% as.matrix()
+  )
   
   # Determine control units to assign to placebo treatment group via matching.
-  matched_placebo_df_temp <- matching_without_replacement(data_wide_m, 
+  matched_placebo_df_temp <- .MatchingWithoutReplacement(data_wide_m, 
                                                           cd_for_match, 
                                                           id_var,
                                                           "Treatment_Period")
-  
   # We now have the set of control units that form the placebo set, along with 
-  #their placebo Treat Period and their corresponding Treat Unit
+  # their placebo Treat Period and their corresponding Treat Unit.
   # Assign treatment to our donor only tibble based on matches.
   placebo_df_wide <- cd_for_match %>%
     dplyr::left_join(matched_placebo_df_temp, by = id_var)
@@ -191,10 +171,6 @@ create_placebo_df <- function(data_full, id_var = "entry",
   # Pivot_long the tibble and recreate the treat_indicator indicator
   # equal to 1 if unit i in time t is treated, 
   # so that the output matches the input.
-  # If there are covariates or other variables which we do not want to be 
-  # pivoting, append them to non_time_vars vectors
-  # may have to add values_to as well if we have multiple time varying 
-  # variables (added as TODO)
   non_time_vars <- c(id_var, "Treatment_Period", "Treatment_Unit")
   placebo_df_long <- placebo_df_wide %>%
     tidyr::pivot_longer(-all_of(non_time_vars),
