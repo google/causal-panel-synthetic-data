e18b52e0941e90ee7ba56db212f1d27d85cb10e2
Alex Kellogg <alexkellogg@google.com>
2020-08-21 17:48:56 -0700

    Updates based on first round of feedback from oldham in PR#9.

--- r_code/seed_metrics.R
+++ r_code/seed_metrics.R
@@ -1,33 +1,29 @@
 pacman::p_load(dplyr, furrr, tidyr, resample, tibble)
 
-#' Computes the bias of the ATT.
+#' Computes the bias of the average treatment effect on the treated (ATT).
 #'
-#' @param tot_list List of tibbles, each tibble provides the estimated ATT from
+#' @param att_list List of tibbles, each tibble provides the estimated ATT from
 #'    a given simulated sample from the same DGP. Will work if the input is just
 #'    a single tibble, but this represents just one noisy estimate.
 #' @param horizon Range of periods over which to display the output.
 #' @param time_var String name for the post period column in the input tibble.
-#' @param pred_var String name for the outcome to compute Bias over (e.g. ATT).
+#' @param pred_var String name for the outcome to compute bias over (e.g. ATT).
 #' @param counterfac_var String name of the true outcome.
 #'
 #' @return A tibble with the period, the absolute bias and the percent bias
 #'   estimated for that period over each DGP draw.
-ComputeBias <- function(tot_list, horizon = c(0,4),
+ComputeBias <- function(att_list, horizon = c(0,4),
                         time_var="post_period_t",
                         pred_var="observed_mean_abs_tot",
                         counterfac_var="mean_abs_cf_tot") {
-  stopifnot(length(horizon)==2 & horizon[1]<= horizon[2] )
   # Keep only the periods within the horizon.
-  tot_tib <- tot_list %>%
-    dplyr::bind_rows() %>%
-    dplyr::filter(!!as.name(time_var) <= horizon[2],
-                  !!as.name(time_var) >= horizon[1])
+  att_tib <- .ExtractSubset(att_list = att_list, horizon = horizon)
   # For each period compute the average observed ATT less the average true ATT.
-  bias_tib <- tot_tib %>%
+  bias_tib <- att_tib %>%
     dplyr::group_by(!!as.name(time_var)) %>%
     dplyr::summarise(
-      "bias" = mean(!!as.name(pred_var), na.rm = T) -
-        mean(!!as.name(counterfac_var), na.rm = T)
+      "bias" = mean(!!as.name(pred_var), na.rm = T)
+       - mean(!!as.name(counterfac_var), na.rm = T)
     )
   
   return(bias_tib)
@@ -37,19 +33,14 @@ ComputeBias <- function(tot_list, horizon = c(0,4),
 #'
 #' @inheritParams ComputeBias
 #'
-#' @return A tibble with the period, the variance of the absolute ATT and the
-#'    variance of the percent ATT in that period.
-ComputeVariance <- function(tot_list, horizon = c(0,4),
+#' @return A tibble with the period, the variance of the desired ATT (pred_var).
+ComputeVariance <- function(att_list, horizon = c(0,4),
                             time_var="post_period_t",
                             pred_var="observed_mean_abs_tot") {
-  stopifnot(length(horizon)==2 & horizon[1]<= horizon[2] )
   # Keep only the periods within the horizon.
-  tot_tib <- tot_list %>%
-    dplyr::bind_rows() %>%
-    dplyr::filter(!!as.name(time_var) <= horizon[2],
-                  !!as.name(time_var) >= horizon[1])
+  att_tib <- .ExtractSubset(att_list = att_list, horizon = horizon)
   # For each period compute the variance of the att estimate.
-  var_tib <- tot_tib %>%
+  var_tib <- att_tib %>%
     dplyr::group_by(!!as.name(time_var)) %>%
     dplyr::summarise("var_att" = stats::var(!!as.name(pred_var), na.rm = T))
   
@@ -57,31 +48,28 @@ ComputeVariance <- function(tot_list, horizon = c(0,4),
 }
 
 #' Computes the coverage of the ATT.
-#' @param tot_list List of tibbles, each tibble provides the estimated ATT from
+#' @param att_list List of tibbles, each tibble provides the estimated ATT from
 #'    a given simulated sample from the same DGP. Will run if the input is a
 #'    single tibble, but coverage will vary wildly (either at 0% or 100%).
 #' @inheritParams ComputeBias
+#' @param ub_var String name for the CI upper bound of the ATT of interest.
+#' @param lb_var String name for the CI lower bound of the ATT of interest.
 #'
 #' @return A tibble with the period and the estimate coverage of the ATT CI for
-ComputeCoverage <- function(tot_list, horizon = c(0,4),
+ComputeCoverage <- function(att_list, horizon = c(0,4),
                             time_var="post_period_t",
                             counterfac_var="mean_abs_cf_tot",
                             ub_var="jackknife_ub_mean_abs_tot",
                             lb_var= "jackknife_lb_mean_abs_tot") {
-  stopifnot(length(horizon)==2 & horizon[1]<= horizon[2] )
   # Keep only the periods within the horizon.
-  tot_tib <- tot_list %>%
-    dplyr::bind_rows() %>%
-    dplyr::filter(!!as.name(time_var) <= horizon[2],
-                  !!as.name(time_var) >= horizon[1])
+  att_tib <- .ExtractSubset(att_list = att_list, horizon = horizon)
   # For each period, see how many of the sample CIs contain the truth.
-  coverage_out <- tot_tib %>%
+  coverage_out <- att_tib %>%
     dplyr::group_by(!!as.name(time_var)) %>%
     dplyr::mutate(
-      contains_truth = as.numeric(!!as.name(counterfac_var) <=
-                                    !!as.name(ub_var) &
-                                    !!as.name(counterfac_var) >=
-                                    !!as.name(lb_var))) %>%
+      contains_truth = as.numeric(
+        (!!as.name(counterfac_var) <= !!as.name(ub_var)) &
+        (!!as.name(counterfac_var) >= !!as.name(lb_var)))) %>%
     dplyr::summarise(
       coverage = mean(contains_truth),
     )
@@ -94,18 +82,14 @@ ComputeCoverage <- function(tot_list, horizon = c(0,4),
 #'
 #' @return A tibble with the RMSE and MAE of the ATT over DGP seeds,
 #'    computed per period, for both absolute and percent ATT.
-ComputeMetrics <- function(tot_list, horizon = c(0,4),
+ComputeMetrics <- function(att_list, horizon = c(0,4),
                            time_var="post_period_t",
                            pred_var="observed_mean_abs_tot",
                            counterfac_var="mean_abs_cf_tot") {
-  stopifnot(length(horizon)==2 & horizon[1]<= horizon[2] )
   # Keep only the periods within the horizon.
-  tot_tib <- tot_list %>%
-    dplyr::bind_rows() %>%
-    dplyr::filter(!!as.name(time_var) <= horizon[2],
-                  !!as.name(time_var) >= horizon[1])
-  # For each period compute the mse and mae of the att estimate.
-  metrics_tib <- tot_tib %>%
+  att_tib <- .ExtractSubset(att_list = att_list, horizon = horizon)
+  # For each period compute the rmse and mae of the att estimate.
+  metrics_tib <- att_tib %>%
     dplyr::group_by(!!as.name(time_var)) %>%
     dplyr::summarise(
       "rmse_att" = sqrt(
@@ -121,3 +105,21 @@ ComputeMetrics <- function(tot_list, horizon = c(0,4),
   
   return(metrics_tib)
 }
+
+
+#' Extracts the subset of relevant time periods from the full data.
+#'
+#' @inheritParams ComputeBias
+#'
+#' @return A tibble, combining the list of data from input and returning just
+#'    the relevant periods of time with respect to treatment defined by horizon.
+.ExtractSubset <- function(att_list, horizon = c(0, 4)) {
+  stopifnot(length(horizon)==2 & horizon[1]<= horizon[2] )
+  # Keep only the periods within the horizon.
+  att_tib <- att_list %>%
+    dplyr::bind_rows() %>%
+    dplyr::filter(!!as.name(time_var) <= horizon[2],
+                  !!as.name(time_var) >= horizon[1])
+  return(att_tib)
+}
+
